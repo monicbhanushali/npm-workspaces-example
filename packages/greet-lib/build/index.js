25e38e5f13f5ff9a46fb6d1d89494fb27d1ff8d6
Monic Bhanushali <monic.bhanushali@cimpress.com>
2023-06-10 22:15:11 +0530

    Remove unwanted files and build output

--- packages/greet-lib/build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.greet = void 0;
-function greet() {
-    return 'Hey there! Have a great day :)';
-}
-exports.greet = greet;
